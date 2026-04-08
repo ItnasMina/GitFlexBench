dead766365cb079799057dccb3441a48f9e925ff
ItnasMina <santiago.criadogonzalo@gmail.com>
2026-04-08 10:19:01 +0200

    Cambio en el pinout

--- FlexBench/src/Pinout.hpp
+++ FlexBench/src/Pinout.hpp
@@ -1,7 +1,7 @@
 // --- DEFINICIÓN DE PINES (Lado izquierdo del ESP32-S3) ---
 
-#define STEP_PIN 10
-#define DIR_PIN  11
+#define STEP_PIN 11
+#define DIR_PIN  10
 #define EN_PIN   12
 
 #define TX_PIN   17  // TX del ESP32 (Va al pin RX del driver)
